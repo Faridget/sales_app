f9843d4c8d6c1e4fc3f0a09d812e9db1c02806b7
Faridget <faridget@gmail.com>
2024-05-06 19:27:52 +0300

    06-05-2024

--- lib/features/settings/presentation/view/settings_view.dart
+++ lib/features/settings/presentation/view/settings_view.dart
@@ -1,15 +1,83 @@
 import 'package:flutter/material.dart';
+ 
 
-class _SettingsView extends StatefulWidget {
-  const _SettingsView({super.key});
+class SettingsView extends StatefulWidget {
+  const SettingsView({super.key});
 
   @override
-  State<_SettingsView> createState() => __SettingsViewState();
+  State<SettingsView> createState() => SettingsViewState();
 }
-
-class __SettingsViewState extends State<_SettingsView> {
+   
+class SettingsViewState extends State<SettingsView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Settings')),
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    child: const Text('English'),
+                    onPressed: () {
+                       
+                    },
+                  ),
+                ),
+                const SizedBox(width: 8.0),
+                Expanded(
+                  child: ElevatedButton(
+                    child: const Text('ភាសាខ្មែរ'),
+                    onPressed: () {
+                       
+                    },
+                  ),
+                ),
+                const SizedBox(width: 8.0),
+                Expanded(
+                  child: ElevatedButton(
+                    child: const Text('عربي'),
+                    onPressed: () {
+                       
+                    },
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16.0),
+             
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemWidget extends StatelessWidget {
+  const ItemWidget({
+    super.key,
+    required this.title,
+    required this.content,
+  });
+
+  final String? title;
+  final String? content;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(child: Text(title ?? '')),
+          const Text(' : '),
+          Expanded(child: Text(content ?? '')),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 //import 'package:sales_app/features/companylist/presentation/view/company_list_view.dart';
-import 'package:sales_app/features/login/presentation/view/login_view.dart';
+//import 'package:sales_app/features/login/presentation/view/login_view.dart';
+import 'package:sales_app/features/settings/presentation/view/settings_view.dart';
+import 'package:sales_app/generated/l10n.dart';
 
  
 void main() {
@@ -11,14 +14,36 @@ void main() {
   FlutterNativeSplash.remove();
 }
 
-class MainApp extends StatelessWidget {
-  final Color _primaryColor = const Color.fromARGB(15, 63, 112, 239);
-  final Color _accentColor = const Color.fromARGB(15, 60, 224, 194);
+class MainApp extends StatefulWidget {
+
     const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  final Color _primaryColor = const Color.fromARGB(15, 63, 112, 239);
+
+  final Color _accentColor = const Color.fromARGB(15, 60, 224, 194);
+  
+  @override
+  void initState() {
+     
+     
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return     MaterialApp(
+       localizationsDelegates: const [
+                S.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: S.delegate.supportedLocales,
       debugShowCheckedModeBanner: false,
        theme: ThemeData(
         primaryColor: _primaryColor,
@@ -26,7 +51,7 @@ class MainApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.grey.shade100,
         primarySwatch: Colors.grey,
       ),
-      home: const LoginView(),
+      home: const SettingsView(),
      
     );
   }
